e866eb6c4314d5ec3fa68c9174f4a28dcd1bdf5c
indaam <indaam@gmail.com>
2020-04-27 10:08:26 +0700

    Update default import

--- index.js
+++ index.js
@@ -3,20 +3,26 @@ import FN from './src/utils/fn';
 import base64 from './src/utils/base64.js';
 
 const { SecretConfig } = NativeModules;
-const SecretConfigStr = SecretConfig.getString('');
 
-const generateConfig = SecretConfigStr => {
-  const json = FN.toJson(SecretConfigStr);
-  if (json) {
-    // Normal
-    return json;
+const generateConfig = () => {
+  if(SecretConfig && SecretConfig.getString){
+    const SecretConfigStr = SecretConfig.getString('');
+    const json = FN.toJson(SecretConfigStr);
+    if (json) {
+      // Normal
+      return json;
+    }
+
+    // Hash
+    const decodeStr = base64.atob(SecretConfigStr);
+    return FN.toJson(decodeStr);
   }
-
-  // Hash
-  const decodeStr = base64.atob(SecretConfigStr);
-  return FN.toJson(decodeStr);
+  return {
+    error : 1,
+    msg : "need to setup"
+  }
 };
 
-const finalConfig = generateConfig(SecretConfigStr);
+const finalConfig = generateConfig();
 
 export default finalConfig;
